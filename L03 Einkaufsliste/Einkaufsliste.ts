517a81e72d4c3c6d7d996879ef87425d8ac6f410
alexanderck27 <kettler.ack@gmail.com>
2024-04-20 11:46:41 +0200

    Einkaufsliste

---
+++ L03 Einkaufsliste/Einkaufsliste.ts
@@ -0,0 +1,60 @@
+namespace Einkaufsliste{
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+let Produktname = document.getElementById("Produktname");
+ if (Produktname) {
+    Produktname.addEventListener("input", () => {
+        console.log("Produktname wurde hinzugefügt");
+    });
+}
+
+let Anzahl = document.getElementById("Anzahl");
+ if (Anzahl) {
+    Anzahl.addEventListener("input", () => {
+        console.log("Produktanzahl wurde hinzugefügt");
+    });
+}
+
+let Kommentar = document.getElementById("Kommentar");
+ if (Kommentar) {
+    Kommentar.addEventListener("input", () => {
+        console.log("Kommentar wurde hinzugefügt");
+    });
+}
+
+let Datum = document.getElementById("Datum");
+ if (Datum) {
+    Datum.addEventListener("input", () => {
+        console.log("Datum wurde hinzugefügt");
+    });
+}
+
+let checkbox1 = document.getElementById("checkbox1");
+ if (checkbox1) {
+    checkbox1.addEventListener("input", () => {
+        console.log("Produkt muss eingekauft werden");
+    });
+}
+
+let checkbox2 = document.getElementById("checkbox2");
+ if (checkbox2) {
+    checkbox2.addEventListener("input", () => {
+        console.log("Produkt wurde gekauft");
+    });
+}
+
+let button = document.getElementById("button");
+ if (button) {
+    button.addEventListener("input", () => {
+        console.log("Produkt hinzufügen");
+    });
+}
+
+
+
+
+});
+}
